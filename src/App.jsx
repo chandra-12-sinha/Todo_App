9ef38e33eb2d8efb7f5bb4a79f18f3c048ea8dc6
chandra-12-sinha <chandrakalasinha083@gmail.com>
2024-08-16 18:59:42 +0530

    added some logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 import "./App.css"
 import AddTodo from "./Components/AddTodo/AddTodo"
 import TodoList from './Components/TodoList/TodoList'
 
 function App() {
+  const[list, setList] = useState([
+    { id:1, todoData: 'todo1', finished: false},
+    { id:2, todoData: 'todo2', finished: false},
+    
+
+  ])
   return (
     <div>
-      <h1 className='heading'>
-        Todo App
-        </h1>
+      <h1 className='heading'> Todo App </h1>
         <hr />
-        <AddTodo/>
-    <TodoList/>
+        <AddTodo updateList={(todo)=> setList([
+          ...list,{id: list.length +1, todoData:todo, finished:false}
+        ])}/>
+    <TodoList list={list}/>
     </div>
   )
 }
--- src/Components/AddTodo/AddTodo.jsx
+++ src/Components/AddTodo/AddTodo.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import "./AddTodo.css"
 
-function AddTodo() {
+function AddTodo({updateList}) {
+    const[inputText, setInputText] = useState('');
   return (
     <div className='add-wrapper'>  
-      <input className='input' type="text" placeholder='Enter a new todo..' />
-    <button>Add</button>
+      <input 
+      className='input'
+       type="text"
+        placeholder='Enter a new todo..'
+         value={inputText} 
+         onChange={(e)=>setInputText(e.target.value)}/>
+    <button onClick={()=> updateList(inputText)}>Add</button>
     </div>
   )
 }
