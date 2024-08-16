be996a19fcb93d13b84cf668aa0ea52b91d46208
chandra-12-sinha <chandrakalasinha083@gmail.com>
2024-08-16 19:34:00 +0530

    added some props and logics in todolist

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,19 +1,29 @@
     import React from 'react'
 import Todo from '../Todo/Todo'
     
-    function TodoList(list) {
-        // const list =[
-        //     {
-        //         id: 1, todoData:"todo1"
-        //     },
-        //     {id: 2, todoData: "todo2"}
-        // ]
+    function TodoList({list, setList}) {
+       
        
       return (
         <div>
           {
             list.length>0 && list.map((todo)=>
-            <Todo key={todo.id} todoData={todo.todoData}/>)
+            <Todo key={todo.id} 
+            id={todo.id}
+            todoData={todo.todoData}
+            isFinished={todo.finished} 
+            changeFinished={(isFinished)=>{
+              console.log("isFinished", isFinished);
+              const updateList= list.map((t)=>{
+                if(t.id == todo.id){
+                  todo.finished = isFinished
+                }
+                return t;
+
+              })
+              setList(updateList)
+            }}
+            />)
           }
         </div>
       )
