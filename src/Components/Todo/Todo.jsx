d7cb747f3a20af2ae2e15683f8ae54b784a967f1
chandra-12-sinha <chandrakalasinha083@gmail.com>
2024-08-16 12:43:07 +0530

    create components

---
+++ src/Components/Todo/Todo.jsx
@@ -0,0 +1,16 @@
+
+
+function Todo({todoData}) {
+  return (
+    <div>
+      <input type="checkbox" />
+      {
+        todoData
+      }
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  )
+}
+
+export default Todo
